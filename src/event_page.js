176e3cbc5592b098b20d27cfbaba9eabb0b30f06
pjh5000 <34818103+pjh5000@users.noreply.github.com>
2017-12-28 16:28:28 +0000

    Translate code comments in event_page.js into English (with some additional explanatory text)

--- src/event_page.js
+++ src/event_page.js
@@ -1,13 +1,13 @@
 chrome.browserAction.onClicked.addListener(function(wtf) {
-  // Ouverture 
+  // Open extension's manager.html page in new tab
   chrome.tabs.query({'url':chrome.extension.getURL('manager.html')}, function(tabs) {
-    // Fermeture de tous les onglets courrants
+    // Close any existing tabs which are currently displaying manager.html
     for(var i = 0 ; i < tabs.length ; ++i)
     {
       chrome.tabs.remove(tabs[i]['id']);
     }
-    
-    // Creation de l'onglet
+
+    // Create new tab for manager.html
     chrome.tabs.create({'url': chrome.extension.getURL('manager.html')}, function(tab) {
       // Tab opened.
     });
