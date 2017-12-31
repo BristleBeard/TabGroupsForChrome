64e2d7f6e2d1c9dc935232e3d80dee4066aa8e8b
pjh5000 <34818103+pjh5000@users.noreply.github.com>
2017-12-31 22:17:41 +0000

    Tweaks and improvements to code formatting:
    E.g. adjustments to spacing, line wrapping, increment operator, etc. To improve code readability and consistency.

--- src/browser.js
+++ src/browser.js
@@ -1,16 +1,13 @@
-function createTab(tab, onFirstLaunch, callback)
-{
-  if(!onFirstLaunch)// Do not re-open tabs which are already open
-  {
-      chrome.tabs.create({url:tab.url, active:false, pinned:tab.pinned}, function(createdTab) {
-      // Retrieve the Chrome tab ID
-      tab.id_chrome = createdTab.id;
-      callback();
-    });
-  }
+function createTab(tab, onFirstLaunch, callback) {
+    if( ! onFirstLaunch) { // Do not re-open tabs which are already open
+        chrome.tabs.create({ url: tab.url, active: false, pinned: tab.pinned }, function(createdTab) {
+            // Retrieve the Chrome tab ID
+            tab.id_chrome = createdTab.id;
+            callback();
+        });
+    }
 }
 
-function closeTab(tab)
-{
-  chrome.tabs.remove(tab.id_chrome);
+function closeTab(tab) {
+    chrome.tabs.remove(tab.id_chrome);
 }
--- src/event_page.js
+++ src/event_page.js
@@ -1,15 +1,14 @@
 chrome.browserAction.onClicked.addListener(function(wtf) {
-  // Open extension's manager.html page in new tab
-  chrome.tabs.query({'url':chrome.extension.getURL('manager.html')}, function(tabs) {
-    // Close any existing tabs which are currently displaying manager.html
-    for(var i = 0 ; i < tabs.length ; ++i)
-    {
-      chrome.tabs.remove(tabs[i]['id']);
-    }
+    // Open extension's manager.html page in new tab
+    chrome.tabs.query({ 'url': chrome.extension.getURL('manager.html') }, function(tabs) {
+        // Close any existing tabs which are currently displaying manager.html
+        for(var i = 0; i < tabs.length; i++) {
+            chrome.tabs.remove(tabs[i]['id']);
+        }
 
-    // Create new tab for manager.html
-    chrome.tabs.create({'url': chrome.extension.getURL('manager.html')}, function(tab) {
-      // Tab opened.
+        // Create new tab for manager.html
+        chrome.tabs.create({ 'url': chrome.extension.getURL('manager.html') }, function(tab) {
+            // Tab opened.
+        });
     });
-  });
 });
